53a60d95d5325370fa29e16b1bcd640747ab0ed5
CAFETEIRA-OSTER <yuribernardosie@gmail.com>
2025-06-23 18:43:20 -0300

    teste 1

--- led_counter.v
+++ led_counter.v
@@ -6,5 +6,24 @@ module Counter #(
     output wire [7:0] leds
 );
 
+localparam ONE_SEC = CLK_FREQ / 2; // 1 second at 25 MHz
 
+reg [31:0] counter;
+reg [7:0] leds_counter = 0;
+
+assign leds = leds_counter;
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        counter <= 0;
+        leds_counter <= 0; // Reset LED counter on reset
+    end else begin
+        if (counter >= ONE_SEC - 1) begin
+            leds_counter <= leds_counter + 1; // Increment LED counter
+            counter <= 0; // Reset counter after 1 second
+        end else begin
+            counter <= counter + 1; // Increment counter
+        end
+    end
+end
 endmodule
